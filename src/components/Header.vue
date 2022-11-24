4a75439943aa8e5afc29c7cc6dbe63ee803816ed
GuidoRial <rialguido@gmail.com>
2022-11-23 22:01:20 -0300

    Import fontawesome and add user icon

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@
       <ul class="links">
         <li>Link</li>
         <li>Link</li>
-        <li>Link</li>
+        <li><font-awesome-icon icon="fa-solid fa-user" /></li>
       </ul>
     </div>
   </div>
@@ -17,12 +17,11 @@ export default { name: "header" };
 <style scoped>
 .header {
   background-color: var(--dark-gray);
-  width: 95.9vw;
+  width: 100%;
   height: 4rem;
   color: var(--white);
   display: flex;
-  justify-content: space-between;
-  padding: 0 1rem;
+  justify-content: space-around;
   align-items: center;
   font-weight: 700;
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
 import App from "./App.vue";
 import router from "./router";
 const pinia = createPinia();
 
-createApp(App).use(pinia).use(router).mount("#app");
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faUser);
+
+createApp(App)
+  .use(pinia)
+  .use(router)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
